684855d13d01039ff115ff6e0ce232a833b1f498
Maras Chen <maras0830@gmail.com>
2020-12-16 16:21:46 +0800

    💡 feat: order with vendor data

--- src/Objects/Order.php
+++ src/Objects/Order.php
@@ -30,9 +30,20 @@ class Order extends BaseObject implements CreateInterface, DetailInterface
         'total' => 'required',
         'description' => 'nullable',
         'is_force_create' => 'nullable|boolean',
-        'customer_vendor_uuid' => 'nullable|string',
-        'vendor_uuid' => 'nullable|string',
         'meta_data' => 'nullable|array',
+
+        'vendor.name' => 'nullable|string',
+        'vendor.uuid' => 'nullable|string',
+        'vendor.customer_vendor_uuid' => 'nullable|string',
+        'vendor.email' => 'nullable|email',
+        'vendor.remit_info.country_iso' => 'nullable',
+        'vendor.remit_info.type' => 'nullable',
+        'vendor.remit_info.bank_name' => 'nullable',
+        'vendor.remit_info.bank_subname' => 'nullable',
+        'vendor.remit_info.bank_code' => 'nullable',
+        'vendor.remit_info.bank_subcode' => 'nullable',
+        'vendor.remit_info.bank_account' => 'nullable',
+        'vendor.remit_info.bank_account_name' => 'nullable',
     ];
 
     protected static $list_validator = [
